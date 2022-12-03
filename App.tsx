dea8deacd9c040176f1220c80b718d5a0b70bb96
NizTheDev <arehman08@hotmail.com>
2022-12-03 21:04:36 +0500

    feat: added another dependency

--- App.tsx
+++ App.tsx
@@ -1,11 +1,13 @@
+import { NavigationContainer } from "@react-navigation/native";
 import { StatusBar } from "expo-status-bar";
 import { CategoriesScreen } from "./src/screens/CategoriesScreen/Categories.screen";
-
 export default function App() {
   return (
     <>
       <StatusBar style="dark" />
-      <CategoriesScreen />
+      <NavigationContainer>
+        <CategoriesScreen />
+      </NavigationContainer>
     </>
   );
 }
