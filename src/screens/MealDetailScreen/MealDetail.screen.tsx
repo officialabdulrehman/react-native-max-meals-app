7de433b3bc451d996aad316c03c6e7fc94da0e51
NizTheDev <arehman08@hotmail.com>
2022-12-04 23:13:13 +0500

    feat: some styles to MealDetail screen | moving on ahead to learn more | will continue styling this screen at the end of project

--- src/screens/MealDetailScreen/MealDetail.screen.tsx
+++ src/screens/MealDetailScreen/MealDetail.screen.tsx
@@ -1,7 +1,8 @@
 import { RouteProp, useRoute } from "@react-navigation/native";
-import { Text, View } from "react-native";
+import { Image, Text, View } from "react-native";
 import { Screens } from "../../../config/Screen.enum";
 import Meal from "../../../models/meal";
+import { styles } from "./MealDetail.styles";
 
 type Props = {};
 
@@ -14,11 +15,40 @@ type RouteParams = {
 export const MealDetail = (props: Props) => {
   const route = useRoute<RouteProp<RouteParams, Screens.Meals>>();
   const {
-    meal: { title },
+    meal: {
+      title,
+      imageUrl,
+      duration,
+      complexity,
+      affordability,
+      ingredients,
+      steps,
+    },
   } = route.params;
+
   return (
-    <View>
-      <Text>{title}</Text>
+    <View style={styles.container}>
+      <Image source={{ uri: imageUrl }} style={styles.image} />
+      <Text style={styles.title}>{title}</Text>
+      <View style={styles.details}>
+        <Text style={styles.detailItem}>{duration} minutes</Text>
+        <Text style={styles.detailItem}>{complexity.toUpperCase()}</Text>
+        <Text style={styles.detailItem}>{affordability.toUpperCase()}</Text>
+      </View>
+      <View style={styles.sectionHeadingContainer}>
+        <Text style={styles.sectionHeading}>Ingredients</Text>
+      </View>
+      {ingredients.map((ingredient: string) => (
+        <Text key={ingredient} style={styles.detailText}>
+          {ingredient}
+        </Text>
+      ))}
+      <View style={styles.sectionHeadingContainer}>
+        <Text style={styles.sectionHeading}>Steps</Text>
+      </View>
+      {steps.map((step: string) => (
+        <Text key={step}>{step}</Text>
+      ))}
     </View>
   );
 };
--- src/screens/MealDetailScreen/MealDetail.styles.ts
+++ src/screens/MealDetailScreen/MealDetail.styles.ts
@@ -2,6 +2,48 @@ import { StyleSheet } from "react-native";
 
 export const styles = StyleSheet.create({
   container: {
+    marginBottom: 32
+  },
+  image: {
+    width: '100%',
+    height: 350,
+  },
+  title: {
+    fontFamily: "sans-serif-medium",
+    fontWeight: 'bold',
+    fontSize: 24,
+    margin: 8,
+    textAlign: 'center',
+    color: "rgba(79, 195, 247, 1)",
+  },
+  sectionHeadingContainer: {
 
-  }
+  },
+  sectionHeading: {
+    color: "#111",
+    fontFamily: "sans-serif-medium",
+    fontSize: 18,
+    margin: 6,
+    textAlign: "center"
+  },
+  details: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 8
+  },
+  detailItem: {
+    fontFamily: "sans-serif-medium",
+    marginHorizontal: 8,
+    fontSize: 14
+  },
+  detailText: {
+    color: '#111',
+  },
+  listOuterContainer: {
+    alignItems: 'center',
+  },
+  listContainer: {
+    width: '80%',
+  },
 })
