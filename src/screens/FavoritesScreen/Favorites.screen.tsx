c18ef6c879134a9b6a3e80bd32239859b68b090a
NizTheDev <arehman08@hotmail.com>
2022-12-06 22:26:06 +0500

    feat: styled the fallback view for favorite screen

--- src/screens/FavoritesScreen/Favorites.screen.tsx
+++ src/screens/FavoritesScreen/Favorites.screen.tsx
@@ -1,12 +1,23 @@
 import { useContext } from "react";
+import { Text, View } from "react-native";
 import { MEALS } from "../../../data/dummy-data";
 import { MealList } from "../../components/MealList/MealList";
 import { FavoritesContext } from "../../store/context/favorites.context";
+import { styles } from "./Favorites.styles";
 
 type Props = {};
 
 export const FavoritesScreen = () => {
   const favoriteCtx = useContext(FavoritesContext);
   const meals = MEALS.filter(({ id }) => favoriteCtx.ids.includes(id));
+  if (!meals.length) {
+    return (
+      <View style={styles.fallbackContainer}>
+        <Text style={styles.fallbackText}>
+          You not selected any favorites yet.
+        </Text>
+      </View>
+    );
+  }
   return <MealList meals={meals} />;
 };
--- src/screens/FavoritesScreen/Favorites.styles.ts
+++ src/screens/FavoritesScreen/Favorites.styles.ts
@@ -3,5 +3,16 @@ import { StyleSheet } from "react-native";
 export const styles = StyleSheet.create({
   container: {
 
+  },
+  fallbackContainer: {
+    justifyContent: "center",
+    alignItems: "center",
+    height: "100%",
+  },
+  fallbackText: {
+    fontFamily: "sans-serif-medium",
+    fontSize: 20,
+    color: "rgba(79, 195, 247, 1)",
+    fontStyle: "italic"
   }
 })
